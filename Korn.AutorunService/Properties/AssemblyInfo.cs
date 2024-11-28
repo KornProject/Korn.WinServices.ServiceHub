4f7584ec1025200408bbaa096fc8baab2081fc21
Yotic <yoticcitoy@gmail.com>
2024-11-28 03:22:28 +0300

    Add project files.

---
+++ Korn.AutorunService/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Korn.AutorunService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Korn.AutorunService")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a9acd95-861b-4b7c-99f2-bc03d2d93fa6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Korn.AutorunService/Service1.cs
@@ -0,0 +1,37 @@
+﻿using Korn.Utils.System;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace Korn.AutorunService
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        const string KORN_PATH_VAR_NAME = "KORN_PATH";
+
+        protected override void OnStart(string[] args)
+        {
+            var kornPath = SystemVariablesUtils.GetVariable(KORN_PATH_VAR_NAME);
+            var servicePath = Path.Combine(kornPath, "Service\\bin\\Korn.Service.exe");
+
+            while (true)
+            {
+                if (Process.GetProcessesByName("Korn.Service").Length == 0)
+                    Process.Start(servicePath);
+
+                Thread.Sleep(5000);
+            }
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
